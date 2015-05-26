f8fd287fedd0986c4ffe981d94536876c639a159
Edaurdo Bermudez <eduber94@gmail.com>
2015-05-25 21:55:46 -0500

    3NN

--- cheaptix.py
+++ cheaptix.py
@@ -1,5 +1,5 @@
 import csv
-import seat_geek.py
+# import seat_geek.py
 import pandas as pd
 import math
 import numpy as np
@@ -13,15 +13,17 @@ class Event(object):
 	def __setattr__(self, name, value):
 		self.__dict__[name] = value
 	def __init__(self, name, cprice, rprice, city, dtp, popularity, availtix, classification):
+		self.name = name
 		self.cprice = cprice
 		self.rprice = rprice
 		self.city = city
 		self.dtp = dtp # Days until performance
 		self.popularity = popularity
 		self.availtix = availtix
+		self.classification = classification
 	def __getattr__(self, key):
-		if key == "classification":
-			return self.classification
+		if key == "name":
+			return self.name
 		elif key == "cprice":
 			return self.cprice
 		elif key == "rprice":
@@ -34,30 +36,33 @@ class Event(object):
 			return self.popularity
 		elif key == "availtix":
 			return self.availtix
+		elif key == "classification":
+			return self.classification
 		else:
 			raise AttributeError
 
 
 
 
-def NearestNeighbor(train, Event_input):
+def NearestNeighbor(train):
+	Event_input = Event('test',10,25,'New York',10,10,10,'test_class')
+
 	dataFrame = pd.read_csv(train, na_values=["?"])
-	dataFrameValidate = pd.read_csv(validate)
 
 	attributes = list(dataFrame.columns.values)
 	classification_label = attributes[len(attributes) - 1]
 
-	events = 0 * len(dataFrame)
+	events = []
 
 	for row in range(len(dataFrame)):
-		events[row] = Event(dataFrame[row][0], dataFrame[row][1], dataFrame[row][2], dataFrame[row][3], dataFrame[row][4], dataFrame[row][5], dataFrame[row][6], dataFrame[row][7])
+		events.append(Event(dataFrame.iloc[row][0], dataFrame.iloc[row][1], dataFrame.iloc[row][2], dataFrame.iloc[row][3], dataFrame.iloc[row][4], dataFrame.iloc[row][5], dataFrame.iloc[row][6], dataFrame.iloc[row][7]))
 
 	first_event = events[0]
 	lowest_dist=distance(Event_input, first_event)
 
 	for event in events:
 		dist = distance(event, Event_input)
-		if dist < lowest_dist:
+		if dist <= lowest_dist:
 			lowest_dist = dist
 			choice = event
 
@@ -68,45 +73,47 @@ def NearestNeighbor(train, Event_input):
 
 
 
-	def ThreeNearestNeighbor(train, Event_input):
+def ThreeNearestNeighbor(train):
+	Event_input = Event('test',10,25,'New York',10,10,10,'test_class')
+
 	dataFrame = pd.read_csv(train, na_values=["?"])
-	dataFrameValidate = pd.read_csv(validate)
 
 	attributes = list(dataFrame.columns.values)
 	classification_label = attributes[len(attributes) - 1]
 
-	events = 0 * len(dataFrame)
+	events = []
 
 	for row in range(len(dataFrame)):
-		events[row] = Event(dataFrame[row][0], dataFrame[row][1], dataFrame[row][2], dataFrame[row][3], dataFrame[row][4], dataFrame[row][5], dataFrame[row][6], dataFrame[row][7])
+		events.append(Event(dataFrame.iloc[row][0], dataFrame.iloc[row][1], dataFrame.iloc[row][2], dataFrame.iloc[row][3], dataFrame.iloc[row][4], dataFrame.iloc[row][5], dataFrame.iloc[row][6], dataFrame.iloc[row][7]))
 	
 	first_event = events[0]
-	lowest_dist=distance(Event_input, first_event)
+	lowest_dist=distance(first_event, Event_input)
 
 	neighbors = []
 
 	for i in range(3):
 		for event in events:
 			dist = distance(event, Event_input)
-			if dist < lowest_dist:
+			if dist <= lowest_dist:
 				lowest_dist = dist
 				choice = event
 		neighbors.append(choice)
 		events.remove(choice)
-
 		first_event = events[0]
-		lowest_dist=distance(Event_input, first_event)
+		lowest_dist=distance(first_event, Event_input)
 
 	neighbor_classifications = []
 
-	for i in rage(len(neighbors)):
-		n = neighbors(i)
+	for i in range(len(neighbors)):
+		n = neighbors[i]
 		c = n.classification
 		neighbor_classifications.append(c)
 
 	data=Counter(neighbor_classifications)
 
-	Event_input.classification = data.most_common(1)
+	temp = data.most_common(1)
+
+	Event_input.classification = temp[0][0]
 
 	return Event_input.classification
 
@@ -114,15 +121,16 @@ def NearestNeighbor(train, Event_input):
 
 
 def distance(event1, event2):
-	d0 = (event1.name == event2.name)^2
-	d1 = (event1.cprice - event2.cprice)^2
-	d2 = (event1.rprice - event2.rprice)^2
-	d3 = (event1.dtp - event2.dtp)^2
-	d4 = (event1.popularity - event2.popularity)^2
-	d5 = (event1.availtix - event2.availtix)^2
-	d6 = (event1.city == event2.city)^2
+	# d0 = (event1.name == event2.name)^2
+	d1 = math.pow((event1.cprice - event2.cprice),2)
+	d2 = math.pow((event1.rprice - event2.rprice),2)
+	d3 = math.pow((event1.dtp - event2.dtp),2)
+	d4 = math.pow((event1.popularity - event2.popularity),2)
+	d5 = math.pow((event1.availtix - event2.availtix),2)
+	# d6 = (event1.city == event2.city)^2
 
-	return sqrt(d0 + d1 + d2 + d3 + d4 + d5 + d6)
+
+	return math.sqrt(d1 + d2 + d3 + d4 + d5)
 
 
 
